c790f66eba612a9540cf52915a534568a660ef80
Dave O'Neill <dmo@roaringpenguin.com>
2010-03-09 15:13:44 -0500

    Bump version number to 0.101

--- lib/Sys/Detect/Virtualization.pm
+++ lib/Sys/Detect/Virtualization.pm
@@ -10,11 +10,11 @@ Sys::Detect::Virtualization - Detect if a UNIX system is running as a virtual ma
 
 =head1 VERSION
 
-Version 0.100
+Version 0.101
 
 =cut
 
-our $VERSION = '0.100';
+our $VERSION = '0.101';
 
 use constant {
 	VIRT_KVM       => 'Linux KVM',
