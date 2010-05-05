d8e47374332ce9fa15af5a97a2271b9646ab0e2a
Dave O'Neill <dmo@roaringpenguin.com>
2010-05-05 10:53:00 -0400

    Prep for 0.105 release

--- lib/Sys/Detect/Virtualization.pm
+++ lib/Sys/Detect/Virtualization.pm
@@ -10,11 +10,11 @@ Sys::Detect::Virtualization - Detect if a UNIX system is running as a virtual ma
 
 =head1 VERSION
 
-Version 0.104
+Version 0.105
 
 =cut
 
-our $VERSION = '0.104';
+our $VERSION = '0.105';
 
 use constant {
 	VIRT_KVM       => 'Linux KVM',
